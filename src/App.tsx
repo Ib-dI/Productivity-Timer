068465a5d6f5569ab34bc1d18eff6837b72db818
Ibrahim_DeV <me.devs15@gmail.com>
2024-10-15 14:28:16 +0200

    fin responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import Pomodoro from "./components/Pomodoro";
 export default function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-gray-100">
-      {/* <Timer workDuration={workDuration} breakDuration={breakDuration} />
-      <TimerForm onSetDurations={handleSetDurations}/> */}
     <Pomodoro />
     </div>
   )
